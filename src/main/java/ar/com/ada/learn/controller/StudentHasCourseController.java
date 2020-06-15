5185bde0fbf990ca7a9cd2367aeaaf43e183e97b
vicmaron <vicky.maron@gmail.com>
2020-06-15 16:09:29 -0300

    s

--- src/main/java/ar/com/ada/learn/controller/StudentHasCourseController.java
+++ src/main/java/ar/com/ada/learn/controller/StudentHasCourseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/studentHasCourseController")
+@RequestMapping("/studentHasCourses")
 public class StudentHasCourseController {
 
     @Autowired @Qualifier("studentHasCourseService")
