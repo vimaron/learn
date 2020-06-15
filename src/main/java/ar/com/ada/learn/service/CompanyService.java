b0392ff630d444010e2053977c1a5e1b9e9e43d1
vicmaron <vicky.maron@gmail.com>
2020-06-15 11:31:30 -0300

    app properties

--- src/main/java/ar/com/ada/learn/service/CompanyService.java
+++ src/main/java/ar/com/ada/learn/service/CompanyService.java
@@ -1,17 +1,26 @@
 package ar.com.ada.learn.service;
 
+import ar.com.ada.learn.component.BusinessLogicExceptionComponent;
 import ar.com.ada.learn.model.dto.CompanyDTO;
 import ar.com.ada.learn.model.entity.Company;
+import ar.com.ada.learn.model.entity.CompanyCategory;
 import ar.com.ada.learn.model.mapper.CompanyMapper;
 import ar.com.ada.learn.model.mapper.circular.CompanyCycleMapper;
 import ar.com.ada.learn.model.mapper.circular.CycleAvoidingMappingContext;
+import ar.com.ada.learn.model.repository.CompanyCategoryRepository;
 import ar.com.ada.learn.model.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CompanyService implements Services<CompanyDTO>{
+
+
+    CompanyMapper companyMapper;
+
+    BusinessLogicExceptionComponent businessLogicExceptionComponent;
 
     @Autowired
     @Qualifier("cycleAvoidingMappingContext")
@@ -19,7 +28,8 @@ public class CompanyService implements Services<CompanyDTO>{
 
     private CompanyCycleMapper companyCycleMapper = CompanyCycleMapper.MAPPER;
 
-    private CompanyMapper companyMapper;
+    @Autowired @Qualifier("companyCategoryRepository")
+    private CompanyCategoryRepository companyCategoryRepository;
 
     @Autowired @Qualifier("companyRepository")
     private CompanyRepository companyRepository;
@@ -31,10 +41,25 @@ public class CompanyService implements Services<CompanyDTO>{
 
     @Override
     public CompanyDTO save(CompanyDTO dto) {
+        return null;
+    }
 
-        Company companyToSave = companyCycleMapper.toEntity(dto);
-        Company companySaved = companyRepository.save(companyToSave);
-        CompanyDTO companyDtoSaved = companyMapper.toDto(companySaved);
+    public CompanyDTO save(CompanyDTO dto, Long companyCategoryId, Long typeOfCompanyId) {
+
+        CompanyCategory category = companyCategoryRepository.findById(companyCategoryId);
+        CompanyDTO companyDtoSaved = null;
+
+        if (category == null){
+            businessLogicExceptionComponent.throwExceptionEntityNotFound(
+                    "company_category", companyCategoryId);
+
+        }else {
+            Company companyToSave = companyCycleMapper.toEntity(dto);
+            companyToSave.setCompanyCategory(category);
+            Company companySaved = companyRepository.save(companyToSave);
+            companyDtoSaved = companyMapper.toDto(companySaved);
+        }
+
         return companyDtoSaved;
     }
 
