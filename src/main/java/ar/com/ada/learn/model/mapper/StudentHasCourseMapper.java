d9eeba5b750f5de0262ad6f41281a5eb9b2dfad7
vicmaron <vicky.maron@gmail.com>
2020-06-22 19:37:12 -0300

    all searchs

--- src/main/java/ar/com/ada/learn/model/mapper/StudentHasCourseMapper.java
+++ src/main/java/ar/com/ada/learn/model/mapper/StudentHasCourseMapper.java
@@ -2,8 +2,10 @@ package ar.com.ada.learn.model.mapper;
 
 import ar.com.ada.learn.model.dto.StudentHasCourseDTO;
 import ar.com.ada.learn.model.entity.StudentHasCourse;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper(componentModel = "spring")
 public interface StudentHasCourseMapper extends DataCycleMapper<StudentHasCourseDTO, StudentHasCourse>{
     StudentHasCourseMapper MAPPER = Mappers.getMapper(StudentHasCourseMapper.class);
 }
--- src/main/java/ar/com/ada/learn/model/repository/CourseRepository.java
+++ src/main/java/ar/com/ada/learn/model/repository/CourseRepository.java
@@ -1,14 +1,26 @@
 package ar.com.ada.learn.model.repository;
 
 import ar.com.ada.learn.model.entity.Course;
-import ar.com.ada.learn.model.entity.TypeOfCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository("courseRepository")
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
+    @Query(value = "SELECT * FROM Course c WHERE c.categoryId = :categoryId", nativeQuery = true)
+    List<Course> findAllByCategory(@Param("category") Long categoryId);
+
+    @Query(value = "SELECT * FROM Course c WHERE c.company = :company", nativeQuery = true)
+    List<Course> findAllByCompany(@Param("company") Long companyId);
+
+    @Query(value = "SELECT * FROM Course s WHERE s.student = :student AND isc = isc.status")
+    List<Course> findAllByStudentAndStatus(@Param("studentId") Long studentId, @Param("isClosed") String isClosed);
+
+    @Query(value = "SELECT * FROM Course co WHERE co.companyId = :companyId AND c = c.categoryId")
+    List<Course> findAllByCompanyAndCategory(@Param("companyId") Long companyId, @Param("categoryId") Long categoryId);
     //Optional<Course> findByType(TypeOfCourse type);
 }
